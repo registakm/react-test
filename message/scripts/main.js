716607414f2709a5a842ed014c6aced709eaf45f
murayama-kazuki <murayama-kazuki@cyberagent.co.jp>
2015-06-30 18:57:22 +0900

    子から親への処理の委譲

--- message/scripts/main.js
+++ message/scripts/main.js
@@ -2,18 +2,29 @@ var App = React.createClass({
   getInitialState: function() {
     return { message: '' }
   },
-  updateMessage: function(e) {
-    this.setState({ message: e.target.value });
+  updateMessage: function(message) {
+    this.setState({ message: message });
   },
   render: function() {
     return (
       <div>
-        <input type="text" onChange={this.updateMessage} />
+        <MessageInput onChange={this.updateMessage} />
         <Message message={this.state.message} />
       </div>
     )
   }
 });
+
+var MessageInput = React.createClass({
+  _onChange: function(e) {
+    this.props.onChange(e.target.value);
+  },
+  render: function() {
+    return <input type="text" onChange={this._onChange} />;
+  }
+});
+
+
 
 var Message = React.createClass({
   render: function() {
